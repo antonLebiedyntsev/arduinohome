570130c24b0f3c048d41394a4031fbd43b1ed133
Anton Lebiedyntsev <anton.lebiedyntsev@gmail.com>
2017-11-12 16:14:12 +0200

    Updated with blink function

--- Arduino_light_ik/Arduino_light_ik.ino
+++ Arduino_light_ik/Arduino_light_ik.ino
@@ -8,15 +8,26 @@ IRrecv irrecv(ikPin); // указываем вывод, к которому по
 decode_results results;
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
   irrecv.enableIRIn();
   pinMode(relPin, OUTPUT);
 }
 
+void blink(int times) {
+  for (times; times > 0; times --){
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(40);                       // wait for a second
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      delay(40); 
+  }
+}
 void loop() {
+  
   if ( irrecv.decode( &results )) { // data came
       Serial.println( stat );
       Serial.println( results.value); // print data
+      blink(3);
       if ( results.value == 4294967295 ){
           if (stat == true ){
             digitalWrite(relPin, HIGH);
